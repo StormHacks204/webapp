0bb7605354a370b115b352bfcb98e3deaa9e3e39
Salman Ayaz <salmanayazz@gmail.com>
2024-10-05 23:27:47 -0700

    fixed mongo connection again

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,7 @@ import { Express } from "express";
 import express from "express";
 import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node'
 import postRouter from "./post";
-import cors from "cors";
-import session from "express-session";
 import logger from "morgan";
-import postRouter from "./post";
 import mongoose from "mongoose";
 
 const app: Express = express();
@@ -16,40 +13,18 @@ const port = process.env.PORT || 5000;
 dotenv.config();
 app.use(cors())
 
-app.use(cors())
+app.use(logger("dev"));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-app.use("/posts", ClerkExpressRequireAuth(),postRouter);
-// declare module "express-session" {
-//   interface SessionData {
-//     username?: string;
-//   }
-// }
+console.log(process.env.MONGO_KEY);
+mongoose.connect(process.env.MONGO_KEY || "");
+mongoose.connection.on("open", function (ref) {
+	console.log("Connected to mongo server.");
+});
 
-// app.use(logger("dev"));
-
-// app.use(
-//   session({
-//     name: "session",
-//     secret: process.env.SESSION_SECRET || "secret",
-//     cookie: {
-//       httpOnly: true,
-//       secure: "auto",
-//       maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
-//     },
-//   })
-// );
-
-// console.log(process.env.MONGO_KEY);
-// mongoose.connect(process.env.MONGO_KEY || "");
-// mongoose.connection.on("open", function (ref) {
-// 	console.log("Connected to mongo server.");
-// });
-
-// app.use("/posts", postRouter);
-// >>>>>>> main
+app.use("/posts", ClerkExpressRequireAuth(), postRouter);
 
 export default client;
