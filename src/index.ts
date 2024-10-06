4b2df6fd87d3b891d1a4a6375df5b5922fa5724f
Shabbir Yusufali <shabyusufali@gmail.com>
2024-10-05 19:25:34 -0700

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import { client } from "./database/mongo";
+import cors from "cors";
 import { Express } from "express";
 import express from "express";
 
@@ -8,6 +9,8 @@ import postRouter from "./post";
 dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors())
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
