642abe4574bf4e361284f5fe4ffa80bd9ece1189
Salman Ayaz <salmanayazz@gmail.com>
2024-10-05 19:52:47 -0700

    fixed mongo connection

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import { client } from "./database/mongo";
 import cors from "cors";
 import { Express } from "express";
 import express from "express";
-
+import session from "express-session";
+import logger from "morgan";
 import postRouter from "./post";
+import mongoose from "mongoose";
 
 dotenv.config();
 const app: Express = express();
@@ -14,6 +16,32 @@ app.use(cors())
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+declare module "express-session" {
+  interface SessionData {
+    username?: string;
+  }
+}
+
+app.use(logger("dev"));
+
+app.use(
+  session({
+    name: "session",
+    secret: process.env.SESSION_SECRET || "secret",
+    cookie: {
+      httpOnly: true,
+      secure: "auto",
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+    },
+  })
+);
+
+console.log(process.env.MONGO_KEY);
+mongoose.connect(process.env.MONGO_KEY || "");
+mongoose.connection.on("open", function (ref) {
+	console.log("Connected to mongo server.");
 });
 
 app.use("/posts", postRouter);
